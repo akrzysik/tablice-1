c440c59330a461817997932eb447ffef1f6de393
akrzysik <95127620+akrzysik@users.noreply.github.com>
2021-12-27 18:39:03 +0000

    losuj, potegi dwojki, wielokrotnosci 3

--- Main.java
+++ Main.java
@@ -1,12 +1,37 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
+
+  public static int losuj(int max) {
+    Random rand = new Random();
+    return rand.nextInt(max);
+  }
 
   public static void wczytaj_tablice(int[] tablica, int rozmiar) throws IOException {
     BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
     for (int i = 0; i < rozmiar; i++) {
       System.out.println("Podaj liczbe: ");
       tablica[i] = Integer.parseInt(in.readLine());
+    }
+  }
+
+  public static void losuj_tablice(int[] tablica, int rozmiar, int max) {
+    for (int i = 0; i < rozmiar; i++) {
+      tablica[i] = losuj(max);
+    }
+  }
+
+  public static void potegi_dwojki(int[] tablica, int rozmiar) {
+    for (int i = 0; i < rozmiar; i++) {
+      double pow = Math.pow(2, i);
+      tablica[i] = (int)pow;
+    }
+  }
+
+  public static void dodaj_trojki(int[] tablica, int rozmiar) {
+    for (int i = 0; i < rozmiar; i++) {
+      tablica[i] = i*3;
     }
   }
 
@@ -22,11 +47,30 @@ public class Main {
       System.out.println("Podaj rozmiar: ");
       r = Integer.parseInt(in.readLine());
       int[] t = new int[r];
-      wczytaj_tablice(t, r);
+      //wczytaj_tablice(t, r);
 
-      System.out.println("Podales nastepujace liczby:");
+      //1.a
+      losuj_tablice(t, r, 100);
+      System.out.println("Tablica zawiera nastepujace liczby wylosowane z zakresu 1 - 100:");
+      wypisz_tablice(t, r);
+      int max;
+      System.out.println("Podaj max: ");
+      max = Integer.parseInt(in.readLine());
+      
+      //1.b
+      losuj_tablice(t, r, max);
+      System.out.println("Tablica zawiera nastepujace liczby wylosowane z zakresu 1 - " + max + ":");
       wypisz_tablice(t, r);
 
+      //2.a
+      potegi_dwojki(t, r);
+      System.out.println("Tablica zawiera nastepujace potęgi 2:");
+      wypisz_tablice(t, r);
+
+      //2.b
+      dodaj_trojki(t, r);
+      System.out.println("Tablica zawiera nastepujace wielokrotności 3:");
+      wypisz_tablice(t, r);
     } catch (IOException e) {
     }
   }
